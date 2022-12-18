a15a3b3879b17b3491dda6b62d4eced49a8099f8
Mustafanaji0413 <mustafanaji0413@gmail.com>
2022-12-18 21:24:14 +0000

    Link Home page and About page

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from . import views
-
-urlpatterns = {
+ 
+urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about'),
-}
+    path('about', views.about, name='about')
+]
