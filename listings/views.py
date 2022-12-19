a02decb8cb5d3d7ecfb4a70e40c743a6f550054c
Mustafanaji0413 <mustafanaji0413@gmail.com>
2022-12-19 21:13:40 +0000

    Add dymanic images to each listing

--- listings/views.py
+++ listings/views.py
@@ -1,24 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 
 
 def index(request):
     listings = Listing.objects.order_by('-list_date')
-
     paginator = Paginator(listings, 6)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
-
     context = {
         'listings': paged_listings
     }
-
     return render(request, 'listings/listings.html', context)
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
