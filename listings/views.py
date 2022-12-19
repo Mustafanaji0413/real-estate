92ff547de5b39fb78b84204cfadc813f30637014
Mustafanaji0413 <mustafanaji0413@gmail.com>
2022-12-19 22:47:21 +0000

    Enable serach function to find keywords in listing descriptions

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
 from .models import Listing
 
 
@@ -24,5 +25,18 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
 
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+    if keywords: 
+        queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list,
+    }
+    return render(request, 'listings/search.html', context)
