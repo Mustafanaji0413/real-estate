948327d21ca6bcee9e1de664c54671964ce3622c
Mustafanaji0413 <mustafanaji0413@gmail.com>
2022-12-20 14:28:19 +0000

    Add login finctionality and redirect to the dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 
@@ -40,7 +40,18 @@ def register(request):
 def login(request):
     if request.method == 'POST':
         # login User
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You have succesfully logged in!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     else: 
         return render(request, 'accounts/login.html')
 
