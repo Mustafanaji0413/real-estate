3a8197980513ee1ac8a705c418c885594790e018
Mustafanaji0413 <mustafanaji0413@gmail.com>
2022-12-29 21:53:21 +0000

    Fixing issue in line 75 in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -72,7 +72,7 @@ def logout(request):
 
 def dashboard(request):
     user_contacts = Contact.objects.order_by('-contact_date').filter
-    (user_id= request.user.id)
+    (user_id == request.user.id)
 
     context = {
         'contacts': user_contacts
