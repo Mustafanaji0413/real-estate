2b86cb8336aaa8f05f61cd7c271b3589e4bfcfaf
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-03-01 14:42:33 +0000

    Setting debug to true to solve add listing issue

--- realestate/settings.py
+++ realestate/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+
+#'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['real-estate0413.herokuapp.com', 'localhost']
 
