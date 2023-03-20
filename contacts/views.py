4a5ddebc78b168b2bf8df3ec186a469ebd134342
Mustafanaji0413 <mustafanaji0413@gmail.com>
2023-03-20 15:35:44 +0000

    Changing line to long error

--- contacts/views.py
+++ contacts/views.py
@@ -19,8 +19,7 @@ def contact(request):
         # Check if user already has made a request
         if request.user.is_authenticated:
             user_id = request.user.id
-            has_contacted = Contact.objects.all().filter(listing_id=listing_id,
-                                                         user_id=user_id)
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
             if has_contacted:
                 messages.error
                 (request, 'You have already made an inquiry for this listing.')
